b5998eecbc3b8e98224ef7abdcd032a1bf38c2c1
wangtx13 <zjwangtx13@gmail.com>
2016-01-04 00:30:21 +0800

    change method for calculate potential evaluator and add calculate type weight

--- src/texas/holdem/hand/evaluator/PotentialEvaluator.java
+++ src/texas/holdem/hand/evaluator/PotentialEvaluator.java
@@ -15,40 +15,71 @@ public class PotentialEvaluator {
      * The potential value of hand
      */
     private double potentialValue = 0;
+    
+    private double aboveRoyalFlushWeight = 0;
+    private double aboveStraightFlushWeight = 0;
+    private double aboveFourOfAKindWeight = 0;
+    private double aboveFullHouseWeight = 0;
+    private double aboveFlushWeight = 0;
+    private double aboveStraightWeight = 0;
+    private double aboveThreeOfAKindWeight = 0;
+    private double aboveTwoPairWeight = 0;
+    private double aboveOnePairWeight = 0;
+    private double aboveHighCardWeight = 0;
 
-    /**
-     * The number of cards for each rank
-     */
-//    private int[] rankings = new int[Card.NO_OF_RANKS];
     /**
      * Constructor.
      *
      * @param currentHand The current hand.
      */
     public PotentialEvaluator(Hand currentHand) {
-        Card[] currentCards = currentHand.getCards();
         double weight = 0;
+
+        double totalValue = 0;
+        int[][] rankings = new int[Card.NO_OF_RANKS][Card.NO_OF_SUITS];
+        int[] types = new int[10];
+        for (int i = 0; i < 10; ++i) {
+            types[i] = 0;
+        }
+        Card[] currentCards = currentHand.getCards();
+
+        for (int i = 0; i < currentHand.size(); ++i) {
+            ++rankings[currentCards[i].getRank()][currentCards[i].getSuit()];
+        }
 
         //Calculate the number of cards for each rank
         //Calculate the potential value of current hand
         if (currentHand.size() == 2) {
             weight = 1d / ((52 - 2) * (52 - 5) * (52 - 6));
-            potentialValue = calTotalPotValueBeforeFlap(currentHand) * weight;
+            totalValue = calculatePotentialVAndT(0, 3, currentHand, rankings, types);
         } else if (currentHand.size() == 5) {
             weight = 1d / ((52 - 5) * (52 - 6));
-            potentialValue = calTotalPotValueBeforeTurn(currentHand) * weight;
+            totalValue = calculatePotentialVAndT(0, 2, currentHand, rankings, types);
         } else if (currentHand.size() == 6) {
             weight = (1d / (52 - 6));
-            potentialValue = calTotalPotValueBeforeRiver(currentHand) * weight;
+            totalValue = calculatePotentialVAndT(0, 1, currentHand, rankings, types);
         }
-
+        potentialValue = totalValue * weight;
+        
+        System.out.println(types[9]);
+        
+        double totalTypeNumbers = 0;
+        for(int i = 0; i < 10; ++i) {
+            totalTypeNumbers += types[i];
+        }
+        totalTypeNumbers = 1;
+//        System.out.println(totalTypeNumbers);
+        aboveRoyalFlushWeight = getAimTypes(types, 9) / totalTypeNumbers;
+        aboveStraightFlushWeight = getAimTypes(types, 8) / totalTypeNumbers;
+        aboveFourOfAKindWeight = getAimTypes(types, 7) / totalTypeNumbers;
+        aboveFullHouseWeight = getAimTypes(types, 6) / totalTypeNumbers;
+        aboveFlushWeight = getAimTypes(types, 5) / totalTypeNumbers;
+        aboveStraightWeight = getAimTypes(types, 4) / totalTypeNumbers;
+        aboveThreeOfAKindWeight = getAimTypes(types, 3) / totalTypeNumbers;
+        aboveTwoPairWeight = getAimTypes(types, 2) / totalTypeNumbers;
+        aboveOnePairWeight = getAimTypes(types, 1) / totalTypeNumbers;
+        aboveHighCardWeight = getAimTypes(types, 0) / totalTypeNumbers;
     }
-//    
-//    private int calculateCurrentValue(Hand currentHand) { 
-//        HandEvaluator evaluator = new HandEvaluator(currentHand);       
-//        return evaluator.getValue();
-//    }
-//   
 
     /**
      * Returns the potential value as a double in one situation
@@ -57,87 +88,95 @@ public class PotentialEvaluator {
      *
      * @return the potential value in one situation
      */
-    private double calculateOnePotentialValue(Hand newHand) {
+    private double calculateHandValue(Hand newHand) {
         HandEvaluator evaluator = new HandEvaluator(newHand);
         int cardValue = evaluator.getValue();
-//        System.out.println(cardValue);
         return cardValue;
     }
 
-    private double calTotalPotValueBeforeRiver(Hand currentHand) {
-        double totalValue = 0;
-        int[][] rankings = new int[Card.NO_OF_RANKS][Card.NO_OF_SUITS];
-        Card[] currentCards = currentHand.getCards();
-
-        for (int i = 0; i < currentHand.size(); ++i) {
-            ++rankings[currentCards[i].getRank()][currentCards[i].getSuit()];
-        }
-
-        for (int i = 0; i < Card.NO_OF_RANKS; ++i) {
-            for (int j = 0; j < Card.NO_OF_SUITS; ++j) {
-                if (rankings[i][j] == 0) {
-                    Card newCard = new Card(i, j);
-                    Hand newHand = new Hand(currentHand.toString() + ' ' + newCard.toString());
-//                    System.out.println();
-//                    System.out.println(newHand.toString());
-                    totalValue += calculateOnePotentialValue(newHand);
-                }
-            }
-        }
-
-        return totalValue;
+    private int calculateHandType(Hand newHand) {
+        HandEvaluator evaluator = new HandEvaluator(newHand);
+        int handType = evaluator.getType().getValue();
+        return handType;
     }
 
-    private double calTotalPotValueBeforeTurn(Hand currentHand) {
-        double totalValue = 0;
-        int[][] rankings = new int[Card.NO_OF_RANKS][Card.NO_OF_SUITS];
-        Card[] currentCards = currentHand.getCards();
-
-        for (int i = 0; i < currentHand.size(); ++i) {
-            ++rankings[currentCards[i].getRank()][currentCards[i].getSuit()];
-        }
-
-        for (int i = 0; i < Card.NO_OF_RANKS; ++i) {
-            for (int j = 0; j < Card.NO_OF_SUITS; ++j) {
-                if (rankings[i][j] == 0) {
-                    Card newCard = new Card(i, j);
-                    Hand newHand = new Hand(currentHand.toString() + ' ' + newCard.toString());
-//                    System.out.println();
-//                    System.out.println(newHand.toString());
-                    totalValue += calTotalPotValueBeforeRiver(newHand);
+    private double calculatePotentialVAndT(int roundedTime, int needRounds, Hand currentHand, int[][] rankings, int[] types) {
+        if (roundedTime < needRounds) {
+            double value = 0;
+            for (int i = 0; i < Card.NO_OF_RANKS; ++i) {
+                for (int j = 0; j < Card.NO_OF_SUITS; ++j) {
+                    if (rankings[i][j] == 0) {
+                        Card newCard = new Card(i, j);
+                        Hand newHand = new Hand(currentHand.toString() + ' ' + newCard.toString());
+                        rankings[i][j] = 1;
+                        System.out.println(newHand.toString());                    
+                        value += calculatePotentialVAndT(roundedTime + 1, needRounds, newHand, rankings, types);
+                        rankings[i][j] = 0;
+                    }
                 }
             }
+            return value;
+        } else {
+            types[calculateHandType(currentHand)]++;
+            if(calculateHandType(currentHand)==9) {
+                System.out.println();
+            }
+            return calculateHandValue(currentHand);
         }
-
-        return totalValue;
     }
 
-    private double calTotalPotValueBeforeFlap(Hand currentHand) {
-        double totalValue = 0;
-        int[][] rankings = new int[Card.NO_OF_RANKS][Card.NO_OF_SUITS];
-        Card[] currentCards = currentHand.getCards();
-
-        for (int i = 0; i < currentHand.size(); ++i) {
-            ++rankings[currentCards[i].getRank()][currentCards[i].getSuit()];
+    private double getAimTypes(int[] types, int aimType) {
+        int aimTotalNumber = 0;
+        for(int i  = 9; i >= aimType; --i) {
+            aimTotalNumber += types[i];
         }
-
-        for (int i = 0; i < Card.NO_OF_RANKS; ++i) {
-            for (int j = 0; j < Card.NO_OF_SUITS; ++j) {
-                if (rankings[i][j] == 0) {
-                    Card newCard = new Card(i, j);
-                    Hand newHand = new Hand(currentHand.toString() + ' ' + newCard.toString());
-//                    System.out.println();
-//                    System.out.println(newHand.toString());
-                    totalValue += calTotalPotValueBeforeTurn(newHand);
-                }
-            }
-        }
-
-        return totalValue;
+        return aimTotalNumber;
     }
 
     public double getPotentialValue() {
         return potentialValue;
     }
+
+    public double getAboveRoyalFlushWeight() {
+        return aboveRoyalFlushWeight;
+    }
+
+    public double getAboveStraightFlushWeight() {
+        return aboveStraightFlushWeight;
+    }
+
+    public double getAboveFourOfAKindWeight() {
+        return aboveFourOfAKindWeight;
+    }
+
+    public double getAboveFullHouseWeight() {
+        return aboveFullHouseWeight;
+    }
+
+    public double getAboveFlushWeight() {
+        return aboveFlushWeight;
+    }
+
+    public double getAboveStraightWeight() {
+        return aboveStraightWeight;
+    }
+
+    public double getAboveThreeOfAKindWeight() {
+        return aboveThreeOfAKindWeight;
+    }
+
+    public double getAboveTwoPairWeight() {
+        return aboveTwoPairWeight;
+    }
+
+    public double getAboveOnePairWeight() {
+        return aboveOnePairWeight;
+    }
+
+    public double getAboveHighCardWeight() {
+        return aboveHighCardWeight;
+    }
+    
+    
 
 }
--- src/texas/holdem/main/Main.java
+++ src/texas/holdem/main/Main.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package texas.holdem.main;
-import texas.holdem.hand.evaluator.TexasHoldemHandEvaluator;
+import texas.holdem.hand.evaluator.Hand;
+import texas.holdem.hand.evaluator.HandEvaluator;
+import texas.holdem.hand.evaluator.PotentialEvaluator;
 /**
  *
  * @author wangtianxia1
@@ -17,13 +19,37 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        String currentHand = "As Qs Js Ts 2h";
-        TexasHoldemHandEvaluator evaluator = new TexasHoldemHandEvaluator(currentHand);
+        String currentHand = "As Qs";
+        HandEvaluator handEvaluator = new HandEvaluator(new Hand(currentHand));
+        PotentialEvaluator potEvaluator = new PotentialEvaluator(new Hand(currentHand));
+//        TexasHoldemHandEvaluator evaluator = new TexasHoldemHandEvaluator(currentHand);
         
-        int currentValue = evaluator.getCurrentValue();
-        double potentialValue = evaluator.getPotentialValue();
+        int currentValue = handEvaluator.getValue();
+        double potentialValue = potEvaluator.getPotentialValue();
+        
+        double aboveRoyalFlushWeight = potEvaluator.getAboveRoyalFlushWeight();
+        double aboveStraightFlushWeight = potEvaluator.getAboveStraightFlushWeight();
+        double aboveFourOfAKindWeight = potEvaluator.getAboveFourOfAKindWeight();
+        double aboveFullHouseWeight = potEvaluator.getAboveFullHouseWeight();
+        double aboveFlushWeight = potEvaluator.getAboveFlushWeight();
+        double aboveStraightWeight = potEvaluator.getAboveStraightWeight();
+        double aboveThreeOfAKindWeight = potEvaluator.getAboveThreeOfAKindWeight();
+        double aboveTwoPairWeight = potEvaluator.getAboveTwoPairWeight();
+        double aboveOnePairWeight = potEvaluator.getAboveOnePairWeight();
+        double aboveHighCardWeight = potEvaluator.getAboveHighCardWeight();
+        
         System.out.println("Current Value: " + currentValue);
         System.out.println("Potential Value: " + potentialValue);
+        System.out.println("aboveRoyalFlushWeight: " + aboveRoyalFlushWeight);
+        System.out.println("aboveStraightFlushWeight: " + aboveStraightFlushWeight);
+        System.out.println("aboveFourOfAKindWeight: " + aboveFourOfAKindWeight);
+        System.out.println("aboveFullHouseWeight: " + aboveFullHouseWeight);
+        System.out.println("aboveFlushWeight: " + aboveFlushWeight);
+        System.out.println("aboveStraightWeight: " + aboveStraightWeight);
+        System.out.println("aboveThreeOfAKindWeight: " + aboveThreeOfAKindWeight);
+        System.out.println("aboveTwoPairWeight: " + aboveTwoPairWeight);
+        System.out.println("aboveOnePairWeight: " + aboveOnePairWeight);
+        System.out.println("aboveHighCardWeight: " + aboveHighCardWeight);
        
     }
 }
